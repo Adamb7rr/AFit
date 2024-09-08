ca8371144e7284828b4e8bbca321ee418527e6c1
Adam <171493434+Adamb7rr@users.noreply.github.com>
2024-09-08 10:58:14 +0300

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://66dcd017b00b1b482fd31de9--a-fit.netlify.app/",
+  baseURL: "http://localhost:8080/api/",
 });
 
 export const UserSignUp = async (data) => API.post("/user/signup", data);
